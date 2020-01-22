c6d237ce8069063d3965a63a42dd93c736c9f91f
Jacob Robinson <95jacob07@gmail.com>
2020-01-22 15:39:36 -0800

    no pass btn for now

--- src/components/PressYourLuck/PressYourLuckActionBtns.vue
+++ src/components/PressYourLuck/PressYourLuckActionBtns.vue
@@ -5,20 +5,20 @@
       v-bind:activeTeam="pressYourLuck.activeTeam"
     />
     <ShuffleBtn v-bind:disabled="pressYourLuck.shuffling" />
-    <PassBtn v-bind:disabled="pressYourLuck.shuffling" />
+    <!-- <PassBtn v-bind:disabled="pressYourLuck.shuffling" /> -->
   </div>
 </template>
 
 <script>
 import ActiveTeamToggler from "./ActiveTeamToggler";
 import ShuffleBtn from "./ShuffleBtn";
-import PassBtn from "./PassBtn";
+// import PassBtn from "./PassBtn";
 
 export default {
   name: "PressYourLuckActionBtns",
   components: {
     ActiveTeamToggler,
-    PassBtn,
+    // PassBtn,
     ShuffleBtn
   },
   props: {
